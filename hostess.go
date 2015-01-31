ada8f87bd712368e81a3dee0a7e1aad47b9cd77b
Chris Bednarski <banzaimonkey@gmail.com>
2015-01-30 21:45:20 -0800

    Added default hosts entries for OSX and Ubuntu

--- hostess.go
+++ hostess.go
@@ -6,6 +6,33 @@ import (
 	"os"
 )
 
+const default_osx = `
+##
+# Host Database
+#
+# localhost is used to configure the loopback interface
+# when the system is booting.  Do not change this entry.
+##
+
+127.0.0.1       localhost
+255.255.255.255 broadcasthost
+::1             localhost
+fe80::1%lo0     localhost
+`
+
+const default_linux = `
+127.0.0.1   localhost
+127.0.1.1   HOSTNAME
+
+# The following lines are desirable for IPv6 capable hosts
+::1     localhost ip6-localhost ip6-loopback
+fe00::0 ip6-localnet
+ff00::0 ip6-mcastprefix
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+ff02::3 ip6-allhosts
+`
+
 type Hostname struct {
 	Domain  string
 	Ip      string
