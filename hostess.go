d99bad74758e24bc1eb17c135fb476ddce7ef990
Chris Bednarski <banzaimonkey@gmail.com>
2015-02-01 00:38:28 -0800

    Added more tests and implemented part of the output bits

--- hostess.go
+++ hostess.go
@@ -6,6 +6,8 @@ import (
 	"io/ioutil"
 	"os"
 	"regexp"
+	"sort"
+	"strings"
 )
 
 const default_osx = `
@@ -39,19 +41,31 @@ type Hostname struct {
 	Domain  string
 	Ip      string
 	Enabled bool
+	// Ipv6    bool
 }
 
+func (h *Hostname) Format() string {
+	r := fmt.Sprintf("%s %s", h.Ip, h.Domain)
+	if !h.Enabled {
+		r = "# " + r
+	}
+	return r
+}
+
+// Hostfile represents /etc/hosts (or a similar file, depending on OS), and
+// includes a list of Hostnames. Hostfile includes
 type Hostfile struct {
 	Path  string
 	Hosts map[string]*Hostname
 	data  string
 }
 
+// NewHostFile creates a new Hostfile object from the specified file.
 func NewHostfile(path string) *Hostfile {
 	return &Hostfile{path, make(map[string]*Hostname), ""}
 }
 
-func (h *Hostfile) Read() string {
+func (h *Hostfile) ReadFile(path string) string {
 	data, err := ioutil.ReadFile(h.Path)
 	if err != nil {
 		fmt.Println("Can't read ", h.Path)
@@ -59,14 +73,6 @@ func (h *Hostfile) Read() string {
 	}
 	h.data = string(data)
 	return h.data
-}
-
-func Dump(hostnames []Hostname) string {
-	return ""
-}
-
-func DumpToFile(path string) {
-
 }
 
 var line_parser = regexp.MustCompile(``)
@@ -84,17 +90,69 @@ func parseLine(line string) (Hostname, error) {
 	return hostname, nil
 }
 
-func Read(hostfile string) []Hostname {
+func (h *Hostfile) Read(hostfile string) []Hostname {
 	var hosts = make([]Hostname, 0)
 	return hosts
 }
 
-func (h *Hostfile) ReadFile(path string) {
+// Dump takes the current list of Hostnames in this Hostfile and turns it into
+// a string suitable for use as an /etc/hosts file.
+// Sorting uses the following logic:
+// 1. List is sorted by IP address
+// 2. Commented items are left in place
+// 3. 127.* appears at the top of the list (so boot resolvers don't break)
+// 4. When present, localhost will always appear first in the domain list
+func (h *Hostfile) Format() string {
+	localhost := "127.0.0.1 localhost"
 
+	localhosts := make(map[string][]string)
+	ips := make(map[string][]string)
+
+	// Map domains and IPs into slices of domains keyd by IP
+	// 127.0.0.1 = [localhost, blah, blah2]
+	// 2.2.2.3 = [domain1, domain2]
+	for _, hostname := range h.Hosts {
+		if hostname.Ip[0:4] == "127." {
+			localhosts[hostname.Ip] = append(localhosts[hostname.Ip], hostname.Domain)
+		} else {
+			ips[hostname.Ip] = append(ips[hostname.Ip], hostname.Domain)
+		}
+	}
+
+	out := make([]string, 0)
+	for _, hostname := range h.Hosts {
+		if hostname.Enabled && hostname.Ip == "127.0.0.1" {
+			if hostname.Domain != "localhost" {
+				localhost += " " + hostname.Domain
+			}
+		} else {
+			out = append(out, hostname.Format())
+		}
+
+	}
+	sort.Sort(sort.StringSlice(out))
+	return localhost + "\n" + strings.Join(out, "\n")
 }
 
-func (h *Hostfile) Add(host Hostname) {
-	h.Hosts[host.Domain] = &host
+func (h *Hostfile) Save() error {
+	// h.Format(h.Path)
+	return nil
+}
+
+func (h *Hostfile) Add(host Hostname) error {
+	host_f, found := h.Hosts[host.Domain]
+	if found {
+		if host_f.Ip == host.Ip {
+			return errors.New(fmt.Sprintf("Duplicate hostname entry for %s -> %s",
+				host.Domain, host.Ip))
+		} else {
+			return errors.New(fmt.Sprintf("Conflicting hostname entries for %s -> %s and -> %s",
+				host.Domain, host.Ip, host_f.Ip))
+		}
+	} else {
+		h.Hosts[host.Domain] = &host
+	}
+	return nil
 }
 
 func (h *Hostfile) Delete(domain string) {
--- hostess_test.go
+++ hostess_test.go
@@ -4,6 +4,12 @@ import (
 	"testing"
 )
 
+const asserts = `
+--- Expected ---
+%s
+---- Actual ----
+%s`
+
 const ipv4_pass = `
 127.0.0.1
 127.0.1.1
@@ -53,7 +59,7 @@ func TestGetHostsPath(t *testing.T) {
 }
 
 func TestHostfile(t *testing.T) {
-	hostfile := NewHostfile(GetHostsPath())
+	hostfile := NewHostfile("./hosts")
 	hostfile.Add(Hostname{domain, ip, true})
 	if hostfile.Hosts[domain].Ip != ip {
 		t.Errorf("Hostsfile should have %s pointing to %s", domain, ip)
@@ -73,5 +79,56 @@ func TestHostfile(t *testing.T) {
 	if hostfile.Hosts[domain] != nil {
 		t.Errorf("Did not expect to find %s", domain)
 	}
+}
 
+func TestHostFileDuplicates(t *testing.T) {
+	hostfile := NewHostfile("./hosts")
+
+	const exp_duplicate = "Duplicate hostname entry for localhost -> 127.0.0.1"
+	hostfile.Add(Hostname{domain, ip, true})
+	err := hostfile.Add(Hostname{domain, ip, true})
+	if err.Error() != exp_duplicate {
+		t.Errorf(asserts, exp_duplicate, err)
+	}
+
+	const exp_conflict = "Conflicting hostname entries for localhost -> 127.0.1.1 and -> 127.0.0.1"
+	err2 := hostfile.Add(Hostname{domain, "127.0.1.1", true})
+	if err2.Error() != exp_conflict {
+		t.Errorf(asserts, exp_conflict, err2)
+	}
+
+}
+
+func TestFormatHostname(t *testing.T) {
+	hostname := Hostname{domain, ip, enabled}
+
+	const exp_enabled = "127.0.0.1 localhost"
+	if hostname.Format() != exp_enabled {
+		t.Errorf(asserts, hostname.Format(), exp_enabled)
+	}
+
+	hostname.Enabled = false
+	const exp_disabled = "# 127.0.0.1 localhost"
+	if hostname.Format() != exp_disabled {
+		t.Errorf(asserts, hostname.Format(), exp_disabled)
+	}
+}
+
+func TestFormatHostfile(t *testing.T) {
+	// t.Skip("Not implemented")
+	const expected = `127.0.0.1 localhost devsite
+127.0.1.1 ip-10-37-12-18
+# 8.8.8.8 google.com
+10.37.12.18 devsite.com m.devsite.com`
+
+	hostfile := NewHostfile("./hosts")
+	hostfile.Add(Hostname{"localhost", "127.0.0.1", true})
+	hostfile.Add(Hostname{"ip-10-37-12-18", "127.0.1.1", true})
+	hostfile.Add(Hostname{"devsite", "127.0.0.1", true})
+	hostfile.Add(Hostname{"google.com", "8.8.8.8", false})
+	hostfile.Add(Hostname{"devsite.com", "10.37.12.18", true})
+	hostfile.Add(Hostname{"m.devsite.com", "10.37.12.18", true})
+	if hostfile.Format() != expected {
+		t.Errorf(asserts, expected, hostfile.Format())
+	}
 }
