d1c7217fa485522e493ef3ff6741f867889f4c3e
Chris Bednarski <banzaimonkey@gmail.com>
2015-03-01 18:18:43 -0800

    Added some tests

--- hostlist_test.go
+++ hostlist_test.go
@@ -41,6 +41,19 @@ func TestContainsDomainIp(t *testing.T) {
 	}
 }
 
+func TestFormat(t *testing.T) {
+	hosts := hostess.NewHostlist()
+	hosts.Add(hostess.NewHostname(domain, ip, false))
+	hosts.Add(hostess.NewHostname("google.com", "8.8.8.8", true))
+
+	expected := `# 127.0.0.1 localhost
+8.8.8.8 google.com
+`
+	if hosts.Format() != expected {
+		t.Error("Formatted hosts list is not formatted correctly")
+	}
+}
+
 func TestRemove(t *testing.T) {
 	hosts := hostess.NewHostlist()
 	hosts.Add(hostess.NewHostname(domain, ip, false))
@@ -53,5 +66,26 @@ func TestRemove(t *testing.T) {
 
 	if hosts.ContainsDomain("google.com") {
 		t.Errorf("Expected not to find google.com")
+	}
+}
+
+func RemoveIpv4Domain(t *testing.T) {
+	hosts := hostess.NewHostlist()
+	h1 := hostess.NewHostname("google.com", "127.0.0.1", false)
+	h2 := hostess.NewHostname("google.com", "::1", true)
+	hosts.Add(h1)
+	hosts.Add(h2)
+
+	hosts.RemoveIpv4Domain("google.com")
+	if hosts.ContainsHostname(h1) {
+		t.Error("Should not contain ipv4 hostname")
+	}
+	if !hosts.ContainsHostname(h2) {
+		t.Error("Should still contain ipv6 hostname")
+	}
+
+	hosts.RemoveIpv6Domain("google.com")
+	if len(*hosts) != 0 {
+		t.Error("Should no longer contain any hostnames")
 	}
 }
